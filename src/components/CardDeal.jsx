d1d17c96ed2f2febe745f1410317fb5a623c91bf
lumberlark <slark20@gmail.com>
2023-10-02 22:47:06 -0500

    Finished the project, will be ajusting template and content for repurposing. Great exercise to complete. now to adjust.

--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -9,10 +9,13 @@ const CardDeal = () =>
         <h2 className={styles.heading2}>
         Find a better card deal <br className="sm:block hidden"/> in few easy steps.</h2>
         <p className={`${styles.paragraph} max-w-[470px] mt-5`}>Arcu tortor, purus in mattis at sed integer faucibus. Aliquet quis aliquet eget mauris tortor.ç Aliquet ultrices ac, ametau.</p>
+        <Button styles="mt-10" />
       </div>
-
-      <Button styles="mt-10" />
-
+    <div className={layout.sectionImg}>
+      <img src={card} alt="card" className="w-[100%] h-[100%]"/>
+      
+    </div>
+  
     </section>
   )
 
